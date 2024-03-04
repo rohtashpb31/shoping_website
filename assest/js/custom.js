1de8f404e09d15c7b62201b14db0ac8e38119a98
ROHTASHPB31 <rohtashverma2580@gmail.com>
2024-03-04 22:55:27 +0530

    Added my project

---
+++ assest/js/custom.js
@@ -0,0 +1,20 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
